fa43d4c7be8c67f789b7fccb53c38f14301c6bba
James Latten <james.latten@phntm.com>
2025-03-17 16:15:29 -0400

    Working solution

--- Lab-4/Lab-4/Main.java
+++ Lab-4/Lab-4/Main.java
@@ -9,25 +9,59 @@ public class Main {
       ArrayList<Integer> integerList = Lab4.getList();
       Lab4.outputList(integerList);
 
+      // Start timer
+      long startTime = System.nanoTime();
+
       System.out.println("\n\nBubble sort results ----------------------------------------------");
       ArrayList<Integer> bubbleSortedList = Lab4.bubbleSort(integerList);
       Lab4.outputList(bubbleSortedList);
 
+      // End timer
+      long endTime = System.nanoTime();
+      long duration = (endTime - startTime);
+      System.out.println("\n\nTime taken for bubble sort: " + duration + " nanoseconds");
+
+      // Start timer
+      startTime = System.nanoTime();
+
       System.out.println("\n\nInsertion sort results -------------------------------------------");
       ArrayList<Integer> insertionSortedList = Lab4.insertionSort(integerList);  
       Lab4.outputList(insertionSortedList);
+
+      // End timer
+      endTime = System.nanoTime();
+      duration = (endTime - startTime);
+      System.out.println("\n\nTime taken for insertion sort: " + duration + " nanoseconds");
     }
 }
 
 class Lab4 {
   public static ArrayList<Integer> insertionSort(ArrayList<Integer> integerList) {
     // Step 1 - Implement insertion sort algorithm here
+    for (int i = 1; i < integerList.size(); i++) {
+      int key = integerList.get(i);
+      int j = i - 1;
+      while (j >= 0 && integerList.get(j) > key) {
+        integerList.set(j + 1, integerList.get(j));
+        j = j - 1;
+      }
+      integerList.set(j + 1, key);
+    }
 
     return integerList;
   }
 
   public static ArrayList<Integer> bubbleSort(ArrayList<Integer> integerList) {
     // Step 2 - Implement the bubble sort algorithm here
+    for (int i = 0; i < integerList.size() - 1; i++) {
+      for (int j = 0; j < integerList.size() - i - 1; j++) {
+        if (integerList.get(j) > integerList.get(j + 1)) {
+          int temp = integerList.get(j);
+          integerList.set(j, integerList.get(j + 1));
+          integerList.set(j + 1, temp);
+        }
+      }
+    }
 
     return integerList;
   }
@@ -35,7 +69,7 @@ class Lab4 {
   public static ArrayList<Integer> getList() {
     ArrayList<Integer> integerList = new ArrayList<>();
     String line;
-    try (BufferedReader br = new BufferedReader(new FileReader("integers.txt"))) {
+    try (BufferedReader br = new BufferedReader(new FileReader("./integers.txt"))) {
         while ((line = br.readLine()) != null) {
             integerList.add(Integer.parseInt(line));
         }
